ad25190f4f7e43725548ce98f00657322909553e
denlly <79107345@qq.com>
2017-09-02 20:23:02 +0800

    _layout

--- src/MyTestMVC/Controllers/HomeController.cs
+++ src/MyTestMVC/Controllers/HomeController.cs
@@ -12,13 +12,6 @@ namespace MyTestMVC.Controllers
     {
         public ActionResult Index()
         {
-            Maticsoft.BLL.user user = new Maticsoft.BLL.user();
-
-            Maticsoft.Model.user userObj = new Maticsoft.Model.user(){ id = 1, username = "ssssss", password = "sa123" };
-
-            bool result = user.Add(userObj);
-
-            Console.WriteLine(result);
             return View ();
         }
     }
